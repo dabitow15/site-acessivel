5e32fe345b2ee92eae23b8c104e2860721e52431
dabitow15 <151878488+dabitow15@users.noreply.github.com>
2025-03-05 16:25:43 -0300

    site acessivel

--- acript.js
+++ acript.js
@@ -1,7 +1,17 @@
-function scrollRight() {
-    document.getElementById("carousel").scrollLeft += 220;
-}
+let currentIndex = 0;
 
-function scrollLeft() {
-    document.getElementById("carousel").scrollLeft -= 220;
+function moverCarrossel(direcao) {
+    const carrossel = document.querySelector(".carousel");
+    const totalCards = document.querySelectorAll(".flip-card").length;
+    const cardWidth = document.querySelector(".flip-card").offsetWidth + 20; // Largura do card + espaçamento
+
+    currentIndex += direcao;
+
+    if (currentIndex < 0) {
+        currentIndex = totalCards - 1;
+    } else if (currentIndex >= totalCards) {
+        currentIndex = 0;
+    }
+
+    carrossel.style.transform = `translateX(${-currentIndex * cardWidth}px)`;
 }
