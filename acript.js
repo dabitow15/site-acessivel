f250671b908f314e2db57c056b77676a2c013e69
dabitow15 <151878488+dabitow15@users.noreply.github.com>
2025-03-05 16:56:47 -0300

    site acessivel

--- acript.js
+++ acript.js
@@ -1,16 +1,3 @@
-let index = 0;
-function moverCarrossel(direcao) {
-    const carrossel = document.querySelector(".carousel");
-    const itens = document.querySelectorAll(".flip-card");
-    const total = itens.length;
-    const itemWidth = itens[0].offsetWidth + 20; // Considera o gap de 20px
-
-    index += direcao;
-    if (index < 0) {
-        index = total - 1;
-    } else if (index >= total) {
-        index = 0;
-    }
-
-    carrossel.style.transform = `translateX(-${index * itemWidth}px)`;
-}
+document.getElementById("botao-contato").addEventListener("click", function() {
+    alert("Obrigado pelo interesse! Em breve, mais informações.");
+});
